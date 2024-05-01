eb7bd2fb8e115fddc785dfe7aaa92f9d80ceff81
Sam Shadwell <sshadwell978@gmail.com>
2024-05-01 11:22:57 -0400

    Forked and cloned exercise, added lines to complete exercise 1

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,26 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var r = new Random();
+            var favNumber = r.Next(1, 10);
+
+            Console.WriteLine("Type in a number to guess my favorite number!");
+            var userInput = int.Parse(Console.ReadLine());
+
+            if (userInput < favNumber)
+            {
+                Console.WriteLine("Too low.");
+            }
+            else if (userInput > favNumber)
+            {
+                Console.WriteLine("Too high.");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            } 
+
+
         }
     }
 }
